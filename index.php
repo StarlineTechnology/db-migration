1a24607e0ca675f95588008e05f56a5781249bce
Devs45 <ayushfrontend@gmail.com>
2024-07-14 14:51:52 +0545

    customer information

--- index.php
+++ index.php
@@ -14,6 +14,15 @@ function escape($db, $value) {
     return $db->real_escape_string($value);
 }
 
+// PrestaShop password hash function
+function hashPassword($password) {
+    return password_hash($password, PASSWORD_BCRYPT);
+}
+
+// Default password for migrated users
+$default_password = "default_password";
+$hashed_default_password = hashPassword($default_password);
+
 // Migrate customers
 $customers = $oscommerce_db->query("
                                     SELECT c.customers_id, c.customers_gender, c.customers_firstname, c.customers_lastname, c.customers_email_address, c.customers_login_allowed, c.customers_password, c.customers_telephone, c.customers_fax, a.entry_company, a.entry_company_tax_id, a.entry_street_address, a.entry_postcode, a.entry_city, a.entry_country_id, ci.customers_info_date_account_created
@@ -25,7 +34,7 @@ if ($customers === false) {
     die("Error fetching customers: " . $oscommerce_db->error);
 }
 
-echo "Migrating customers...<br>";
+echo "Migrating customers...<br><br><br>";
 
 // Gender mapping function
 function mapGender($gender) {
@@ -60,64 +69,15 @@ while ($customer = $customers->fetch_assoc()) {
     $fax = escape($prestashop_db, $customer['customers_fax']);
 
     $query = "INSERT INTO ps_customer (id_customer, id_gender, firstname, lastname, email, passwd, date_add, date_upd, active, company, siret, company_name, company_taxid, street_address, post_code, city_name, country_name, telephone_number, fax_number)
-              VALUES ($id_customer, $id_gender, '$firstname', '$lastname', '$email', MD5('$password'), '$date_add', '$date_upd', '$active', '$company', '$siret', '$company', '$taxid', '$street', '$post', '$city', '$country', '$telephone', '$fax')";
+              VALUES ($id_customer, $id_gender, '$firstname', '$lastname', '$email', '$hashed_default_password', '$date_add', '$date_upd', '$active', '$company', '$siret', '$company', '$taxid', '$street', '$post', '$city', '$country', '$telephone', '$fax')";
 
     if (!$prestashop_db->query($query)) {
-        echo "Error inserting customer ID $id_customer: " . $prestashop_db->error . "<br>";
+        echo "Error inserting customer ID $id_customer: " . $prestashop_db->error . "<br><br><br><br>";
     } else {
         echo "Customer ID $id_customer migrated successfully.<br>";
     }
 }
 
-// Migrate addresses
-$addresses = $oscommerce_db->query("SELECT ab.address_book_id, ab.customers_id, ab.entry_firstname, ab.entry_lastname, ab.entry_street_address, ab.entry_postcode, ab.entry_city, ab.entry_country_id 
-                                    FROM address_book ab");
-
-if ($addresses === false) {
-    die("Error fetching addresses: " . $oscommerce_db->error);
-}
-
-echo "Migrating addresses...<br>";
-while ($address = $addresses->fetch_assoc()) {
-    $id_address = $address['address_book_id'];
-    $id_customer = $address['customers_id'];
-    $firstname = escape($prestashop_db, $address['entry_firstname']);
-    $lastname = escape($prestashop_db, $address['entry_lastname']);
-    $address1 = escape($prestashop_db, $address['entry_street_address']);
-    $postcode = escape($prestashop_db, $address['entry_postcode']);
-    $city = escape($prestashop_db, $address['entry_city']);
-    $id_country = $address['entry_country_id'];
-    
-    $query = "INSERT INTO ps_address (id_address, id_customer, firstname, lastname, address1, postcode, city, id_country, date_add)
-              VALUES ($id_address, $id_customer, '$firstname', '$lastname', '$address1', '$postcode', '$city', $id_country, NOW())";
-
-    if (!$prestashop_db->query($query)) {
-        echo "Error inserting address ID $id_address: " . $prestashop_db->error . "<br>";
-    } else {
-        echo "Address ID $id_address migrated successfully.<br>";
-    }
-}
-
-// Optional: Migrate customer groups if relevant to your setup
-$customer_groups = $oscommerce_db->query("SELECT customers_id, customers_groups_id FROM customers_groups");
-if ($customer_groups === false) {
-    die("Error fetching customer groups: " . $oscommerce_db->error);
-}
-
-echo "Migrating customer groups...<br>";
-while ($group = $customer_groups->fetch_assoc()) {
-    $id_customer = $group['customers_id'];
-    $id_group = $group['customers_groups_id'];
-
-    $query = "INSERT INTO ps_customer_group (id_customer, id_group)
-              VALUES ($id_customer, $id_group)";
-
-    if (!$prestashop_db->query($query)) {
-        echo "Error inserting customer group ID $id_group for customer ID $id_customer: " . $prestashop_db->error . "<br>";
-    } else {
-        echo "Customer group ID $id_group for customer ID $id_customer migrated successfully.<br>";
-    }
-}
 
 echo "Migration completed.";
 
